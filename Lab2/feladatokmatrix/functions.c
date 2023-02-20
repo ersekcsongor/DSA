15c4b35c08cd1320db9e50620699ce75879dc26d
ersekcsongor <ersek.csongor@student.ms.sapientia.ro>
2023-02-20 11:40:02 +0200

    matrix feladat beolvasas es allokacio

--- Lab2/feladatokmatrix/functions.c
+++ Lab2/feladatokmatrix/functions.c
@@ -10,6 +10,7 @@ void allocateMemoryForMatrix2(int rows, int cols, int ***dpMatrix){
         printf(MEMORY_ALLOCATION_ERROR_MESSAGE);
         exit(MEMORY_ALLOCATION_ERROR);
     }
+
     for (int i = 0; i < rows; ++i) {
         (*dpMatrix)[i] = (int*) calloc(cols,sizeof(int));
         if(!*dpMatrix[i])
@@ -22,7 +23,7 @@ void allocateMemoryForMatrix2(int rows, int cols, int ***dpMatrix){
 }
 
 void readMatrix(int *pRows, int *pCols, int ***dpArray, const char *input){
-    allocateMemoryForMatrix2(Cols,,dpArray);
+    allocateMemoryForMatrix2(pRows,pCols,dpArray);
     for (int i = 0; i < pRows; ++i) {
         for (int j = 0; j < pCols; ++j) {
             scanf("%i",dpArray[i][j]);
--- Lab2/feladatokmatrix/main.c
+++ Lab2/feladatokmatrix/main.c
@@ -3,5 +3,6 @@
 int main() {
     int n,m,**a;
     scanf("%i%i",&n,&m);
+    readMatrix(&n,&m,&a,"CON");
     return 0;
 }
