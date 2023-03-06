c658a098aee02f6ad5cda053481ce4de82845ba3
ersekcsongor <ersek.csongor@student.ms.sapientia.ro>
2023-03-06 10:26:05 +0200

    Labor3

---
+++ Lab3/Students/constants.h
@@ -0,0 +1,15 @@
+//
+// Created by Csongor on 2/20/2023.
+//
+#include "student.h"
+
+#ifndef STUDENTS2_CONSTANTS_H
+#define STUDENTS2_CONSTANTS_H
+
+static const int MEMORY_ALLOCATION_ERROR_CODE = -1;
+static const char MEMORY_ALLOCATION_MESSAGE[] = " sikeres allokacio \n";
+static const char MEMORY_ALLOCATION_ERROR_MESSAGE[] = " sikertelen allokacio \n";
+static const int FILE_OPENING_ERROR_CODE = -2;
+static const char FILE_OPENING_ERROR_MESSAGE[] = "sikertelen megnyitas\n";
+
+#endif //STUDENTS2_CONSTANTS_H
