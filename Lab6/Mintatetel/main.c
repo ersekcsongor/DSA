3a79adadbbd69d6ab8d73702eb7a528f4e814d10
ersekcsongor <ersek.csongor@student.ms.sapientia.ro>
2023-03-20 11:41:40 +0200

    Labor6

---
+++ Lab6/Mintatetel/main.c
@@ -0,0 +1,21 @@
+#include "patient.h"
+
+int main() {
+    Patient_t patient1 = {
+            "Pityiri Palko",
+            "197822376",
+            {1996,10,12},
+            DEPRESSION
+    };
+    FILE *out = fopen("CON","w");
+    printPatient(patient1,out);
+    Patient_t patient2;
+    FILE *in = fopen("CON", "r");
+    readPatient(&patient2,in);
+    fclose(in);
+    char fileName[20];
+    out = fopen("CNP.txt","w");
+    printPatient(patient2,out);
+    fclose(out);
+    return 0;
+}
